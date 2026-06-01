9f0e84df30e6649dfb64e5acf4ec60214a0149d7
PrinceOfVerdansk <thominho11@gmail.com>
2026-06-01 19:49:37 +0200

    number checker,EvenOdd

---
+++ Conditionals/EvenOdd.py
@@ -0,0 +1,11 @@
+'''
+    Even or Odd - Determine if a number is even or odd 
+
+'''
+
+number = int(input("Enter a number: "))
+if number % 2 == 0:
+    print(f"{number} is even.")
+else:
+    print(f"{number} is odd.")
+    
