264146b3b89b0a565fc2dbb4650e1090607422c1
PrinceOfVerdansk <thominho11@gmail.com>
2026-06-02 08:18:43 +0200

    Loops,Countdown,number printer

---
+++ Loops/Countdown.py
@@ -0,0 +1,9 @@
+'''
+Countdown - Count down from 10 to 1 using a while loop 
+'''
+
+count = 10
+while count > 0:
+    print(count)
+    count -= 1
+print("Liftoff!")   
---
+++ Loops/NumberPrinter.py
@@ -0,0 +1,5 @@
+'''
+ Number Printer - Print numbers from 1 to 10 using a for loop
+'''
+for number in range(1, 11):
+    print(number)
